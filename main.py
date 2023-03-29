1fe8ad2ce4802c0f3588bebfaa11d6328dec0ba8
KG <KeilGriesLP@gmail.com>
2023-03-29 14:40:01 -0400

    Added total price, company name, logo.

--- main.py
+++ main.py
@@ -10,21 +10,25 @@ for filepath in filepaths:
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
 
+    # Create invoice number and date objects
     filename = Path(filepath).stem
     invoice_nr = filename.split('-')[0]
     date = filename.split('-')[1]
 
+    # Add invoice number header
     pdf.set_font(family='Times', size=16, style='B')
     pdf.cell(w=50, h=8, txt=f'Invoice nr. {invoice_nr}', ln=1)
 
+    # Add date header
     pdf.set_font(family='Times', size=16, style='B')
     pdf.cell(w=50, h=8, txt=f'Date: {date}', ln=1)
 
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
 
-    # Add a header
+    # Add table headers
     columns = list(df.columns)
     columns = [item.replace('_', ' ').title() for item in columns]
+
     pdf.set_font(family='Times', size=10)
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
@@ -33,6 +37,7 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
+    # Add table with data from files
     for index, row in df.iterrows():
         pdf.set_font(family='Times', size=10)
         pdf.set_text_color(80, 80, 80)
@@ -41,6 +46,25 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    # Add total price to table
+    total_sum = df['total_price'].sum()
+    pdf.set_font(family='Times', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=70, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.cell(w=30, h=8, txt=f'The total price is {total_sum}', ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family='Times', size=10, style='B')
+    pdf.cell(w=20, h=8, txt=f'PythonHow')
+    pdf.image('pythonhow.png', w=10)
 
 
     pdf.output(f'PDFs/{filename}.pdf')
